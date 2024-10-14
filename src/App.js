07f4f02f0905cc6249072fab127dfd778037eadd
devashish1997sharma <devashishsharma444@gmail.com>
2024-10-14 07:54:20 +0530

    check update

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Hello World
+          Hello World Azure demo
         </p>
         <a
           className="App-link"
