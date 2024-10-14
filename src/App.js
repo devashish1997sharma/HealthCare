9f2a43c26f537112928c84066d2dc7442e631dd8
devashish1997sharma <devashishsharma444@gmail.com>
2024-10-14 08:46:05 +0530

    working on Login

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
+import Login from './screens/login/login';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
+      {/* <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
           Hello World Azure demo
@@ -17,7 +18,8 @@ function App() {
         >
           Learn React
         </a>
-      </header>
+      </header> */}
+      <Login />
     </div>
   );
 }
